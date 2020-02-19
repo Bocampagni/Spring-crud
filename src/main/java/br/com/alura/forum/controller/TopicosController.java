ed2cdd96b8d518f5d68805cb89b12fddaa7d8159
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-18 21:33:43 -0300

    preventing losts

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,64 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	
+	// @RequestMapping(value="/topicos", method= RequestMethod.GET) Isso pode ir pra de baixo do RestController para melhorar a manutenção
+	
+	@GetMapping //Ai usa-se só o mapeamento com o verbo http
+	public List<TopicoDto> lista(String nomeCurso){
+		if(nomeCurso == null) {
 
-	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		}
+		else {
+			
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 		
-		Topico topico = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+	
+	
+	}
+	
+	
+	//TopicoDto -> Sai da api e vai pro cliente, TopicoForm -> Sai do cliente e vai pra API
+	@PostMapping
+	public void cadastrar(TopicoForm topico) {
 		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
