300a09b466522eca85edecbf0cbc08630c43694a
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-25 18:47:57 -0300

    Adding security

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,15 @@
 package br.com.alura.forum.controller;
 
+import org.springframework.data.domain.Pageable;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -41,15 +44,20 @@ public class TopicosController {
 	// @RequestMapping(value="/topicos", method= RequestMethod.GET) Isso pode ir pra de baixo do RestController para melhorar a manutenção
 	
 	@GetMapping //Ai usa-se só o mapeamento com o verbo http
-	public List<TopicoDto> lista(String nomeCurso){
+	public Page<TopicoDto> lista(@RequestParam(required = false)  String nomeCurso,
+								 @RequestParam int pagina,
+								 @RequestParam int quantidade){
+
+		Pageable paginacao = PageRequest.of(pagina,quantidade);
+
 		if(nomeCurso == null) {
 
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
 		}
 		else {
 			
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.converter(topicos);
 		}
 		
@@ -77,11 +85,9 @@ public class TopicosController {
 	@GetMapping("/{id}")
 	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
 		
-		
 		Optional<Topico> topico = topicoRepository.findById(id);
 		if( topico.isPresent()) {
 			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));   //.get() vai pegar o topico dentro do optional
-			
 		}
 		
 		return ResponseEntity.notFound().build();
